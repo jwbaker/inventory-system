8f92355c6388428ea7f8631c3cdff99ac7f06df6
Jason Baker <jwbaker@uwaterloo.ca>
2015-02-27 16:41:48 -0500

    Minor styling of import form

--- uw_file_io/forms.py
+++ uw_file_io/forms.py
@@ -2,4 +2,10 @@ from django import forms
 
 
 class ImportForm(forms.Form):
-    file_up = forms.FileField()
+    file_up = forms.FileField(required=True)
+    model = forms.ChoiceField(
+        choices=(
+            ('II', 'Inventory Items'),
+        ),
+        label='Record kind'
+    )
