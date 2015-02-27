70e221407574f391b20254d7a4aa71ef389edc7d
Jason Baker <jwbaker@uwaterloo.ca>
2015-02-27 13:24:31 -0500

    Added uw_file_io app and moved file viewing/downloading into it

---
+++ uw_file_io/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from uw_file_io import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^download/(?P<file_id>\d+)/$', views.file_download),
+    url(r'^(?P<file_name>files/.+)/$', views.file_view),
+)
