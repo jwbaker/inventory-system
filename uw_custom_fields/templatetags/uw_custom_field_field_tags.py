a37b7fbb21fc0c14172b7dc2805b402803e83b4f
Jason Baker <jwbaker@uwaterloo.ca>
2015-03-18 11:34:16 -0400

    Added support for custom date fields, and removed custom datetime fields due to poor browser widget support

--- uw_custom_fields/templatetags/uw_custom_field_field_tags.py
+++ uw_custom_fields/templatetags/uw_custom_field_field_tags.py
@@ -14,38 +14,63 @@ def show_custom_fields(field_data_string):
         field_list = []
     return {'fields': field_list}
 
+FIELD_TYPE_TO_ICON = {
+    'email': 'envelope',
+    'password': 'key',
+    'date': 'calendar',
+    'datetime': 'clock-o',
+}
+
 
 def __text_widget_render(field_data):
-    if field_data['type-secondary'] == 'textarea':
-        widget_string = '<textarea class="{classes}">{value}</textarea>'
+    if field_data.get('type-secondary', '') == 'textarea':
+        return_string = '<textarea class="{classes}">{value}</textarea>'
     else:
-        widget_string = '<input type="{0}" class="{1}" value="{2}" />'.format(
-            field_data['type-secondary'] or 'text',
+        return_string = '<input type="{0}" class="{1}" value="{2}" />'.format(
+            field_data.get('type-secondary', '') or field_data['type'] or'text',
             '{classes}',
             '{value}'
         )
 
-    if field_data['type-secondary'] in ['email', 'password']:
-        widget_string = '''<div class="input-group item-input">
+    if field_data.get('type-secondary', '') in ['email', 'password']:
+        return_string = '''<div class="input-group item-input">
          <span class="input-group-addon"><i class="fa fa-{0}"></i>
          </span>{1}</div>'''.format(
-            'envelope' if field_data['type-secondary'] == 'email' else 'key',
-            widget_string.format(
+            FIELD_TYPE_TO_ICON[field_data['type-secondary']],
+            return_string.format(
+                classes='form-element form-control',
+                value=field_data.get('value', '')
+            )
+        )
+    elif field_data['type'] == 'date':
+        return_string = '''<div class="input-group item-input">
+         <span class="input-group-addon"><i class="fa fa-{0}"></i>
+         </span>{1}</div>'''.format(
+            FIELD_TYPE_TO_ICON[field_data['type']],
+            return_string.format(
                 classes='form-element form-control',
                 value=field_data.get('value', '')
             )
         )
     else:
-        widget_string = widget_string.format(
+        return_string = return_string.format(
             classes='form-element form-control item-input',
             value=field_data.get('value', '')
         )
-    return widget_string
+    return return_string
+
+
+def __date_widget_render(field_data):
+    pass
 
 
 @register.simple_tag
 def render_custom_widget(field_data):
     if field_data['type'] == 'text':
         widget_string = __text_widget_render(field_data)
+    elif field_data['type'] == 'date':
+        widget_string = __text_widget_render(field_data)
+    else:
+        widget_string = ''
 
     return mark_safe(widget_string)
