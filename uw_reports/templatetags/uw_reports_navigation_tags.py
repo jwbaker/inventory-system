7b2f7d7c9b24e03e186cf97425416d4ca149b95f
Jason Baker <jwbaker@uwaterloo.ca>
2015-04-01 11:03:36 -0400

    Added permission guard to report facilities

--- uw_reports/templatetags/uw_reports_navigation_tags.py
+++ uw_reports/templatetags/uw_reports_navigation_tags.py
@@ -8,12 +8,18 @@ register = template.Library()
 
 @register.inclusion_tag('uw_reports/context_menu.html')
 def reports_context_menu(user):
-    reports = Report.objects.filter(
-        Q(creator=user) |
-        Q(creator__isnull=True)
-    ).order_by('-view_count', '-creation_date')[:5]
+    if user.has_perm('uw_reports.view_all_reports'):
+        reports = Report.objects.all().order_by(
+            '-view_count', '-creation_date'
+        )[:5]
+    else:
+        reports = Report.objects.filter(
+            Q(creator=user) |
+            Q(creator__isnull=True)
+        ).order_by('-view_count', '-creation_date')[:5]
 
     return {
         'user': user,
-        'reports': reports
+        'reports': reports,
+        'can_add': user.has_perm('uw_reports.add_report')
     }
