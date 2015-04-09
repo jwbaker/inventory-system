0a6b684382ce962c6990d1d69fde7b2e7fa88673
Jason Baker <jwbaker@uwaterloo.ca>
2015-04-09 10:02:39 -0400

    Finally got report exports working

--- uw_inventory/views.py
+++ uw_inventory/views.py
@@ -14,7 +14,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
-from django_cas.decorators import permission_required, user_passes_test
+from django_cas.decorators import permission_required
 
 from uw_inventory.forms import (
     CommentForm,
